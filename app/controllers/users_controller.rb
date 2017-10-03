70eebb3b85df882393f7934331024281005db281
Lucas Murray <lucas.murray2014@gmail.com>
2017-10-03 01:46:32 +0000

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def user_params
-    params.require(:user).permit(:user_id, :email, :session_token)
+    params.require(:user).permit(:user_id, :email)
   end
 
 #   def show
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
+    # @user = User.create!(user_params)
     flash[:notice] = "#{@user.user_id} was successfully created."
     redirect_to movies_path
   end
