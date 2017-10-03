11c2ad08ce2540534b0cf52615793c74f9db35e4
Lucas Murray <lucas.murray2014@gmail.com>
2017-10-03 03:23:49 +0000

    ;lkj

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,11 @@ class UsersController < ApplicationController
 
   def create
     dummyUser = User.create!(user_params)
+    userNames = Array.new
     User.all.each do |user|
-      puts(user[:user_id])
+      userNames.push(user[:user_id])
     end
-    if (User.all.any?{ |user| user[:user_id].eql?(dummyUser.user_id)}) 
+    if (userNames.include?(dummyUser.user_id)) 
       flash[:notice] = "Sorry, this user-id is taken. Try again."
     else
       @user = User.create!(user_params)
